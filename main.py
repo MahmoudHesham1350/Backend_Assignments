12b56c47b0314c92e0251edae242fdff62040275
seba ibraheem <sebaalmahdy@gmail.com>
2025-03-19 22:02:01 +0200

    week 1 assignment

---
+++ main.py
@@ -0,0 +1,48 @@
+with open("input.txt",'r') as file:
+    for line in file:
+        
+        line = line.strip()  # remove any newline character ('\n')
+        
+        # validate if the operation operands and operation is correct or not 
+        if len(line)!=3 or not line[0].isdigit() or not line[2].isdigit() or not line[1] in '+-/*':
+            print("invalid input")
+
+            # clarify if there are any error in the operation and wrte in the file
+            with open("output.txt",'a') as output:
+                output.write("this operation is not valid "+'\n')
+            
+            # continue the other operations
+            continue
+
+        """  line[0]   line[1]   line[2]
+                |         |         |
+            [operand][operation][operand]
+        """
+        error = False
+        operation = line[1]
+        match operation:
+            case '+':
+                ans = int(line[0])+int(line[2])
+            case '-':
+                ans = int(line[0])-int(line[2])
+            case '*':
+                ans = int(line[0])*int(line[2])
+            case '/':
+                # validate that there is no zero division
+                if int(line[2]) == 0:
+                    error = True
+                    with open('output.txt','a') as output:
+                        output.write("Invalid,you divide by zero in this operation\n")
+        
+                if not error:
+                    ans = int(line[0])/int(line[2])
+
+             
+        if not error:
+            with open('output.txt','a') as output:
+                output.write(line + "= "+str(ans)+'\n')
+            
+print("the operations were calculated successfully")
+        
+    
+    
