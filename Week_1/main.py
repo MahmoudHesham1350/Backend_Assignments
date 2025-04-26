739f462b611750d4582f6a60b88d9f3755c48f91
seba3102005 <143961537+seba3102005@users.noreply.github.com>
2025-04-26 20:04:42 +0300

    Update main.py

--- Week_1/main.py
+++ Week_1/main.py
@@ -1,10 +1,23 @@
+# erase any content in the output.txt
+with open('output.txt', 'w') as file:
+    pass 
+
+
+
 with open("input.txt",'r') as file:
     for line in file:
         
         line = line.strip()  # remove any newline character ('\n')
+        parts = line.split(' ',3)   # split(separator, maxsplit)
+        
+
+        # if the user enters an empty line
+        if len(parts)==1 and parts[0]=='':
+            continue
+        
         
         # validate if the operands and operation are correct or not 
-        if len(line)!=3 or not line[0].isdigit() or not line[2].isdigit() or not line[1] in '+-/*':
+        if  not len(parts) == 3 or not parts[0].isdigit() or not parts[2].isdigit() or not parts[1] in '+-/*':
             print("invalid input")
 
             # clarify if there are any error in the operation and write in the file
@@ -14,28 +27,28 @@ with open("input.txt",'r') as file:
             # continue the other operations
             continue
 
-        """  line[0]   line[1]   line[2]
+        """  parts[0]   parts[1]   lineparts[2]
                 |         |         |
             [operand][operation][operand]
         """
         error = False
-        operation = line[1]
+        operation = parts[1]
         match operation:
             case '+':
-                ans = int(line[0])+int(line[2])
+                ans = int(parts[0])+int(parts[2])
             case '-':
-                ans = int(line[0])-int(line[2])
+                ans = int(parts[0])-int(parts[2])
             case '*':
-                ans = int(line[0])*int(line[2])
+                ans = int(parts[0])*int(parts[2])
             case '/':
                 # validate that there is no zero division
-                if int(line[2]) == 0:
+                if int(parts[2]) == 0:
                     error = True
                     with open('output.txt','a') as output:
                         output.write("Invalid,you divide by zero in this operation\n")
         
                 if not error:
-                    ans = int(line[0])/int(line[2])
+                    ans = int(parts[0])/int(parts[2])
 
              
         if not error:
